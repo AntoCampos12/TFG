5aa18a4d3a51c1388e4e21cae8963742f23e7057
AntoCampos12 <drapionx1@gmail.com>
2023-06-12 12:40:05 +0200

    Configuración base de datos MySQL

--- comunio/settings.py
+++ comunio/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'comunio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tfg',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': 3307
     }
 }
 
